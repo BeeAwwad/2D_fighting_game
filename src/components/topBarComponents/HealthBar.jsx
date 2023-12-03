cda3397992e5342b8af169d943bfc65651e61ac2
BeeAwwad <aosalihu@gmail.com>
2023-12-03 08:56:23 +0100

    Turned HealthBar into a pure function

--- src/components/topBarComponents/HealthBar.jsx
+++ src/components/topBarComponents/HealthBar.jsx
@@ -1,26 +1,10 @@
-import { useEffect, useState } from "react"
-
 function HealthBar({ fighter }) {
-  console.log(fighter)
-  const [fighterHealth, setFighterHealth] = useState(fighter.health)
-  const [healthBar, setHealthBar] = useState(calculateHealthBar(fighter.health))
+  const fighterHealth = fighter.health
+  const healthBar = calculateHealthBar(fighterHealth)
 
   function calculateHealthBar(health) {
     return Math.floor(health / 10) * 10
   }
-
-  useEffect(() => {
-    setFighterHealth((prevHealth) => {
-      if (prevHealth !== fighter.health) {
-        return fighter.health
-      }
-      return prevHealth
-    })
-  }, [fighter.health])
-
-  useEffect(() => {
-    setHealthBar(calculateHealthBar(fighterHealth))
-  }, [fighterHealth])
 
   return (
     <div className="relative h-8 w-full">
